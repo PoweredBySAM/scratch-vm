a467c3f9f8115e186ec13ecd134b06283c2a1ace
Rbel12b <150368027+Rbel12b@users.noreply.github.com>
2025-03-08 10:21:32 +0000

    feat: new extension

---
+++ src/extensions/samlabs/index.js
@@ -0,0 +1,345 @@
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+require('regenerator-runtime/runtime');
+
+const SamLabsBLE = {
+    battServ: '0000180f-0000-1000-8000-00805f9b34fb',
+    batteryLevelCharacteristic: '00002a19-0000-1000-8000-00805f9b34fb',
+    SAMServ: '3b989460-975f-11e4-a9fb-0002a5d5c51b',
+    SensorCharacteristic: '4c592e60-980c-11e4-959a-0002a5d5c51b',
+    ActorCharacteristic: '84fc1520-980c-11e4-8bed-0002a5d5c51b',
+    StatusLedCharacteristic: '5baab0a0-980c-11e4-b5e9-0002a5d5c51b'
+}
+
+class Scratch3SamLabs {
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.deviceMap = new Map(); // Store multiple devices
+        this.numberOfConnectedDevices = 0;
+        this.extensionId = 'samlabsExtension';
+        this.runtime.on('PROJECT_STOP_ALL', this.stopAll.bind(this));
+        this.deviceMenu = [];
+        this.blocks = [
+            {
+                opcode: 'connectToDevice',
+                blockType: BlockType.COMMAND,
+                text: 'Connect to a block',
+            },
+            {
+                opcode: 'setLEDColor',
+                blockType: BlockType.COMMAND,
+                text: 'Set Block [num] Status Led Color: R[red], G[green], B[blue]',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER },
+                    red: { defaultValue: 0, type: ArgumentType.NUMBER },
+                    green: { defaultValue: 0, type: ArgumentType.NUMBER },
+                    blue: { defaultValue: 0, type: ArgumentType.NUMBER }
+                }
+            },
+            {
+                opcode: 'setLEDRGBColor',
+                blockType: BlockType.COMMAND,
+                text: 'Set Block [num] RGB Led Color: R[red], G[green], B[blue]',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER },
+                    red: { defaultValue: 0, type: ArgumentType.NUMBER },
+                    green: { defaultValue: 0, type: ArgumentType.NUMBER },
+                    blue: { defaultValue: 0, type: ArgumentType.NUMBER }
+                }
+            },
+            {
+                opcode: 'setBlockMotorSpeed',
+                blockType: BlockType.COMMAND,
+                text: 'Set Block [num] motor speed [val]',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER },
+                    val: { defaultValue: 0, type: ArgumentType.NUMBER }
+                }
+            },
+            {
+                opcode: 'setBlockServo',
+                blockType: BlockType.COMMAND,
+                text: 'Set Block [num] Servo angle [val]°',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER },
+                    val: { defaultValue: 0, type: ArgumentType.NUMBER }
+                }
+            },
+            {
+                opcode: 'getSensorValue',
+                blockType: BlockType.REPORTER,
+                text: 'Sensor value, Block [num]',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER }
+                }
+            },
+            {
+                opcode: 'getBattery',
+                blockType: BlockType.REPORTER,
+                text: 'Battery percentage, Block [num]',
+                terminal: false,
+                arguments: {
+                    num: { menu: 'deviceMenu', type: ArgumentType.NUMBER }
+                }
+            }
+        ];
+
+        this.colors = [
+            "#FF00FF", "#00FFFF", "#FFFF00", "#808000",
+            "#FF0000", "#00FF00", "#0000FF"
+        ];
+    }
+
+    hexToRgb(hex) {
+        hex = hex.replace(/^#/, ""); // Remove "#" if present
+        if (hex.length === 3) {
+            // Convert short hex (e.g. #F00) to full hex (#FF0000)
+            hex = hex.split("").map(c => c + c).join("");
+        }
+        let r = parseInt(hex.substring(0, 2), 16);
+        let g = parseInt(hex.substring(2, 4), 16);
+        let b = parseInt(hex.substring(4, 6), 16);
+        return { r, g, b };
+    }
+
+
+    getInfo() {
+        return {
+            id: this.extensionId,
+            name: 'SAM Labs',
+            showStatusButton: false,
+            color1: '#0FBD8C',
+            color2: '#0DA57A',
+            blocks: this.blocks,
+            menus: {
+                deviceMenu: 'getDeviceMenu'
+            }
+        };
+    }
+
+    updateDeviceMenu() {
+        this.deviceMenu = [];
+        this.deviceMap.forEach(device => {
+            this.deviceMenu.push({ text: device.name, value: device.num });
+        });
+    }
+
+    getDeviceMenu() {
+        return this.deviceMenu.length ? this.deviceMenu : [{ text: 'No connected Blocks', value: '-1' }];
+    }
+
+    addBlock(newBlock) {
+        this.blocks.push(newBlock);
+        this.runtime._refreshExtensions(); // Force a refresh of the extension
+    }
+
+    stopAll() {
+        this.deviceMap.forEach(this.stopDevice.bind(this));
+    }
+
+    stopDevice(device) {
+        this.setLEDRGBColor({ num: device.num, red: 0, green: 0, blue: 0 });
+    }
+
+    async connectToDevice() {
+        try {
+            // Request a Bluetooth device with the specified filter
+            const device = await navigator.bluetooth.requestDevice({
+                filters: [{
+                    namePrefix: 'SAM', // Filter by device name starting with 'SAM'
+                }],
+                optionalServices: [
+                    SamLabsBLE.battServ,
+                    SamLabsBLE.SAMServ
+                ]
+            });
+
+            console.log('Device found:', device);
+
+            device.addEventListener('gattserverdisconnected', () => this.onDisconnected(device));
+
+            // Connect to the GATT server
+            const server = await device.gatt.connect();
+            console.log('Connected to GATT server');
+
+            await this.setupGATTDevice(server, device);
+
+        } catch (error) {
+            console.log('Error:', error);
+        }
+    }
+
+    async setupGATTDevice(server, device) {
+        const num = this.numberOfConnectedDevices;
+        this.numberOfConnectedDevices++;
+        // Get the Battery Service
+        const battServ = await server.getPrimaryService(SamLabsBLE.battServ);
+        console.log('Battery Service found:', battServ);
+
+        // Get the Battery Level Characteristic
+        const batteryLevelCharacteristic = await battServ.getCharacteristic(SamLabsBLE.batteryLevelCharacteristic);
+        console.log('Battery Level Characteristic found:', batteryLevelCharacteristic);
+
+        const SAMServ = await server.getPrimaryService(SamLabsBLE.SAMServ);
+
+        var SAMSensorCharacteristic = null;
+        var SensorAvailable = true;
+
+        try {
+            SAMSensorCharacteristic = await SAMServ.getCharacteristic(SamLabsBLE.SensorCharacteristic);
+        } catch (error) {
+            console.log('Sensor characteristic not found');
+            SensorAvailable = false;
+        }
+        var SAMActorCharacteristic = null;
+        var ActorAvailable = true;
+
+        try {
+            SAMActorCharacteristic = await SAMServ.getCharacteristic(SamLabsBLE.ActorCharacteristic);
+        } catch (error) {
+            console.log('Actor characteristic not found');
+            ActorAvailable = false;
+        }
+
+        const SAMStatusLEDCharacteristic = await SAMServ.getCharacteristic(SamLabsBLE.StatusLedCharacteristic);
+
+        var sameDevices = 1;
+        this.deviceMap.forEach(value => {
+            if (value.device.name == device.name) {
+                sameDevices++;
+            }
+        });
+        let displayName = device.name.substring(4) + ' ' + sameDevices;
+        let block = {
+            num: num,
+            device: device,
+            name: displayName,
+            battReadNotifyCharacteristic: batteryLevelCharacteristic,
+            SAMSensorCharacteristic: SAMSensorCharacteristic,
+            SensorAvailable: SensorAvailable,
+            ActorAvailable: ActorAvailable,
+            SAMActorCharacteristic: SAMActorCharacteristic,
+            SAMStatusLEDCharacteristic: SAMStatusLEDCharacteristic,
+            value: 0,
+            battery: 0
+        };
+
+        this.deviceMap.set(num, block);
+        this.updateDeviceMenu();
+        this.setBlockLedColor(block, this.hexToRgb(this.colors[num]));
+
+        if (SensorAvailable) {
+            await SAMSensorCharacteristic.startNotifications();
+            SAMSensorCharacteristic.addEventListener('characteristicvaluechanged', this.handleSensorNotifications.bind(this, num));
+        }
+
+        await batteryLevelCharacteristic.startNotifications();
+        batteryLevelCharacteristic.addEventListener('characteristicvaluechanged', this.handleBattChange.bind(this, num));
+
+
+        console.log(`Connected to ${device.name || 'Unknown Device'}, num ${num}`);
+    }
+
+    onDisconnected(event) {
+    }
+
+    handleSensorNotifications(num, event) {
+        const value = event.target.value;
+        let device = this.deviceMap.get(num);
+        device.value = value.getUint8(0);
+    }
+
+    handleBattChange(num, event) {
+        const value = event.target.value;
+        let device = this.deviceMap.get(num);
+        device.battery = value.getUint8(0);
+    }
+
+    async setLEDColor(args) {
+        const num = Number(args.num);
+        const block = this.deviceMap.get(num);
+        if (!block) {
+            return;
+        }
+        await this.setBlockLedColor(block, { r: args.red, g: args.green, b: args.blue });
+    }
+
+    async setBlockLedColor(block, color) {
+        let message = new Uint8Array([
+            color.r,
+            color.g,
+            color.b
+        ]);
+        await block.SAMStatusLEDCharacteristic.writeValue(message);
+    }
+
+    async setLEDRGBColor(args) {
+        const num = Number(args.num);
+        const block = this.deviceMap.get(num);
+        if (!block || !block.ActorAvailable) {
+            return;
+        }
+
+        let message = new Uint8Array([
+            args.red,
+            args.green,
+            args.blue
+        ]);
+        await block.SAMActorCharacteristic.writeValue(message);
+    }
+
+    async setBlockMotorSpeed(args) {
+        const block = this.deviceMap.get(Number(args.num));
+        if (!block || !block.ActorAvailable) {
+            return;
+        }
+        let speed = Number(args.val)
+        if (speed < 0) {
+            if (speed < -100) {
+                speed = -100;
+            }
+            speed = Math.abs(speed) * 1.27 + 128
+        }
+        else {
+            if (speed > 100) {
+                speed = 100;
+            }
+            speed = speed * 1.27
+        }
+        let message = new Uint8Array([speed]);
+        await block.SAMActorCharacteristic.writeValue(message);
+    }
+
+    async setBlockServo(args) {
+        const block = this.deviceMap.get(Number(args.num));
+        if (!block || !block.ActorAvailable) {
+            return;
+        }
+        let speed = Number(args.val)
+        let message = new Uint8Array([speed]);
+        await block.SAMActorCharacteristic.writeValue(message);
+    }
+
+    getSensorValue(args) {
+        const block = this.deviceMap.get(Number(args.num));
+        if (!block) {
+            return 0;
+        }
+        return block.value;
+    }
+
+    getBattery(args) {
+        const block = this.deviceMap.get(Number(args.num));
+        if (!block) {
+            return 0;
+        }
+        return block.battery;
+    }
+}
+
+module.exports = Scratch3SamLabs;
